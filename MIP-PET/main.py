edf29407048ab0de37f2563ef186689292a9431d
aastier <arthur.astier37@gmail.com>
2023-12-29 11:21:34 +0100

    - Improved architecture for reading .nii images and converting them to MIP format.

--- MIP-PET/main.py
+++ MIP-PET/main.py
@@ -1,24 +1,10 @@
 # @author Arthur Astier
 
-import os
-
-import nibabel
-
-import nifty_to_mip
+from nifty_to_mip import create_mip_from_path
 
 if __name__ == "__main__":
-    for root, dir, filenames in os.walk("patients"):
-        for filename in filenames:
-            file = os.path.join(root, filename)
-            # filter only image files with the following format
-        if filenames and file.endswith('.nii'):
-            img = nibabel.load(file)
-            split_root = root.split("/")
-            mip_directory = split_root[0] + "/" + split_root[1] + "/" + "MIP" + "/" + split_root[2]
-            if "Mask" in split_root:
-                mask = True
-            else:
-                mask = False
-            nifty_to_mip.create_mip_from_3d(img, patient_reference=mip_directory, nb_image=1, duration=0.1,
-                                            is_mask=mask,
-                                            borne_max=None)
+    patient_folder = "patients"
+    pet_folder = "Original"
+    mask_folder = "Mask"
+    create_mip_from_path(patient_folder, None, mask_folder, pet_borne_max=10, mask_borne_max=None, nb_image=1,
+                         img_size=(512, 512))
--- MIP-PET/nifty_to_mip.py
+++ MIP-PET/nifty_to_mip.py
@@ -1,14 +1,17 @@
 # @author paul-bd and Arthur Astier
 # https://github.com/paul-bd/MIP-PET.git
 import os
-import shutil
+import sys
 import matplotlib.pylab as plt
 import numpy as np
 import scipy
 from scipy import ndimage
+import nibabel
 
 
-def create_mip_from_3d(img, patient_reference: str, nb_image=40, duration=0.1, is_mask=False, borne_max=None):
+def create_mip_from_3d(img, record_directory: str, patient_reference: str, nb_image=40, img_size=(512, 512),
+                       is_mask=False,
+                       borne_max=None):
     ls_mip = []
 
     img_data = img.get_fdata()
@@ -22,20 +25,58 @@ def create_mip_from_3d(img, patient_reference: str, nb_image=40, duration=0.1, i
         MIP[MIP < 1e-5] = 0
         MIP = np.flipud(MIP.T)
         ls_mip.append(MIP)
-    try:
-        shutil.rmtree(patient_reference + '/')
-    except:
-        pass
-    os.mkdir(patient_reference + '/')
 
     for mip, i in zip(ls_mip, range(len(ls_mip))):
         fig, ax = plt.subplots()
+        plt.rcParams["figure.figsize"] = img_size
+        plt.rcParams["figure.dpi"] = 1
         ax.set_axis_off()
         if borne_max is None:
-            if is_mask == True:
+            if is_mask:
                 borne_max = 1
             else:
                 borne_max = 15000
         plt.imshow(mip, cmap='Greys', vmax=borne_max)
-        fig.savefig(patient_reference + '/MIP' + '%04d' % (i) + '.png')
+        fig.savefig(record_directory + '/MIP_' + patient_reference + '%03d' % (i) + '.png', dpi=1)
         plt.close(fig)
+
+
+def create_mip_from_path(patient_folder_name: str, pet_folder_name: str, mask_folder_name: str, pet_borne_max=None,
+                         mask_borne_max=None,
+                         img_size=(512, 512), nb_image=1):
+    if not os.path.exists(os.path.join(os.getcwd(), patient_folder_name)):
+        print('Patient folder provided does not exist', file=sys.stderr)
+    if not os.path.exists(os.path.join(os.getcwd(), patient_folder_name, 'MIP')):
+        os.mkdir(os.path.join(os.getcwd(), patient_folder_name, 'MIP'))
+    if pet_folder_name is not None:
+        generate_from_path(patient_folder_name, pet_folder_name, mask=False, borne_max=pet_borne_max, img_size=img_size,
+                           nb_image=nb_image)
+    if mask_folder_name is not None:
+        generate_from_path(patient_folder_name, mask_folder_name, mask=True, borne_max=mask_borne_max, img_size=img_size,
+                           nb_image=nb_image)
+    print("MIP images generated !")
+
+
+def generate_from_path(patient_folder_name: str, file_folder_name: str, mask=False, borne_max=None, img_size=(512, 512),
+                       nb_image=1):
+    files_path = os.path.join(os.getcwd(), patient_folder_name, file_folder_name)
+    if not os.path.exists(files_path):
+        print('The' + file_folder_name + ' folder provided does not exist', file=sys.stderr)
+
+    pet_files = [pet for pet in os.listdir(files_path) if os.path.isfile(os.path.join(files_path, pet))]
+    if not pet_files:
+        print('The' + file_folder_name + ' folder provided does not contain any file', file=sys.stderr)
+
+    mip_directory = patient_folder_name + '/Test' + '/' + file_folder_name
+    if not os.path.exists(os.path.join(os.getcwd(), mip_directory)):
+        os.mkdir(mip_directory + '/')
+
+    for pet in pet_files:
+        file = os.path.join(files_path, pet)
+        if pet.endswith('.nii'):
+            img = nibabel.load(file)
+            patient_name = pet.split(".")[0]
+            create_mip_from_3d(img, record_directory=mip_directory, patient_reference=patient_name,
+                               nb_image=nb_image, img_size=img_size,
+                               is_mask=mask,
+                               borne_max=borne_max)
