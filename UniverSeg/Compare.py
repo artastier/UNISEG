6e7790820f0cea3bdd0aa3e6f2b5b7b96836f695
aastier <arthur.astier37@gmail.com>
2023-12-30 15:23:55 +0100

    - Create a pop-up for path selection - Modification of Compare to display the intersection of the ground-truth   mask and the predicted mask in orange - Tried to parallelize the segmentation using Pool but without success   (maybe due to access competition with PyTorch)

--- UniverSeg/Compare.py
+++ UniverSeg/Compare.py
@@ -13,23 +13,30 @@ def compare(ground_truth_path: str, segmenter: Segmenter):
         test_mask_file = os.path.join(segmenter.test_folder_path, filename)
         if not os.path.isfile(gt_mask_file) or not os.path.isfile(test_mask_file):
             print("\n We can neither find the ground truth mask nor the test image \n", file=sys.stderr)
-            return None
+            continue
         gt_mask = io.imread(gt_mask_file, as_gray=True).astype(float)
         test_img = io.imread(test_mask_file, as_gray=True).astype(float)
         predicted_mask = segmenter.segmented_images[idx]
         if gt_mask.shape != predicted_mask.shape:
             print("\n The shape of the ground truth mask doesn't match with the shape of the predicted mask \n",
                   file=sys.stderr)
+            continue
         enhanced_images.append(color_enhanced(test_img, gt_mask, predicted_mask))
     return enhanced_images
 
 
 def color_enhanced(test_image, gt_mask, predicted_mask):
     enhanced_image = color.gray2rgb(test_image).copy()
+
     gt_mask_idx = np.where(gt_mask == 0)
     gt_mask_nb_pixels = gt_mask_idx[0].shape
     enhanced_image[gt_mask_idx] = np.repeat(np.array([[0, 1.0, 0]]), gt_mask_nb_pixels, axis=0)
+
     predicted_mask_idx = np.where(predicted_mask == 0)
     predicted_mask_nb_pixels = predicted_mask_idx[0].shape
     enhanced_image[predicted_mask_idx] = np.repeat(np.array([[1.0, 0, 0]]), predicted_mask_nb_pixels, axis=0)
+
+    intersection_idx = np.where(gt_mask+predicted_mask == 0)
+    intersection_nb_pixels = intersection_idx[0].shape
+    enhanced_image[intersection_idx] = np.repeat(np.array([[1.0, 1.0, 0]]), intersection_nb_pixels, axis=0)
     return enhanced_image
--- UniverSeg/main.py
+++ UniverSeg/main.py
@@ -1,18 +1,29 @@
-import os
-
+from PyQt5.QtWidgets import QApplication
+from PathSelection import PathSelection
+import sys
 import matplotlib.pyplot as plt
 from Support import Support
 from Segmenter import Segmenter
 from Compare import compare
 
 if __name__ == "__main__":
+    app = QApplication(sys.argv)
     batch_size = 1
-    support = Support("Support/Original", "Support/Mask", support_size=20, batch_size=batch_size, invert_label=True)
+    map_path = PathSelection("Select the Map folder").directory
+    label_path = PathSelection("Select the Label folder").directory
+    # For my laptop, I have SIGKILL because the support_size is too big and generates too much sub-images
+    support = Support(map_path, label_path, support_size=22, batch_size=batch_size, invert_label=True)
     print("Support size (number of sub-images): {}".format(support.maps.shape[1]))
-    segmenter = Segmenter("Test/Original", support)
-    enhanced_images = compare("Test/Mask", segmenter=segmenter)
-    for image in enhanced_images:
+
+    test_path = PathSelection("Select the Test folder").directory
+    ground_truth_path = PathSelection("Select the Ground Truth Masks of the Test images folder").directory
+    segmenter = Segmenter(test_path, support)
+    enhanced_images = compare(ground_truth_path, segmenter=segmenter)
+    for image, filename in zip(enhanced_images, segmenter.filenames):
         fid, ax = plt.subplots()
+        plt.rcParams["figure.figsize"] = (512, 512)
+        plt.rcParams["figure.dpi"] = 1
         ax.set_axis_off()
         plt.imshow(image, cmap='gray')
+        plt.title(filename)
     plt.show()
