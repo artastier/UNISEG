7304bd0fd27fb1bfc73280f4fada4cb3ea8261b0
damien-gautier-nantes <144633393+damien-gautier-nantes@users.noreply.github.com>
2024-01-24 21:19:15 +0100

    ajout du bouton save et affichages

--- points_prompt_generation.py
+++ points_prompt_generation.py
@@ -18,17 +18,22 @@ import time
 
 # Compare ground_truth and prediction
 from Compare import color_enhanced
+from colorama import Fore
+import os
+import datetime
 
 
 #######
 # Les masks sorties par SAM sont de même dimension que l'image d'entrée, ils sont codés en booléen
 # c'est à dire que chaque pixel prend une valeur soit False(Background) soit True(Mask)
 #
-# Les
+
 class App:
 
     def __init__(self):
         # Variables for storing the prompts
+        self.fig = None
+        self.img_points_selected = None
         self.img_gray = None
         self.gt_mask = None
         self.mask_path = None
@@ -50,16 +55,19 @@ class App:
         # Adding all the components
         self.frame = Frame(self.window)
         # The button for prompting points
-        points_button = Button(self.frame, text="Selectionner les points", font=("Courrier", 25), bg='white',
+        points_button = Button(self.frame, text="Select points to prompt", font=("Courrier", 25), bg='white',
                                fg='#41B77F',
                                command=self.points_selection)
         points_button.pack(pady=25, fill=X)
         # The button for predicting
-        predict_button = Button(self.frame, text="Prédire", command=self.predict)
+        predict_button = Button(self.frame, text="Predict", command=self.predict)
         predict_button.pack()
         # The button for choosing the image
-        img_choice_button = Button(self.frame, text="Load une image", command=self.img_choice)
+        img_choice_button = Button(self.frame, text="Load an image", command=self.img_choice)
         img_choice_button.pack()
+        # The button for saving the result
+        img_save_button = Button(self.frame, text="Save the prediction", command=self.img_save)
+        img_save_button.pack()
         self.frame.pack(expand=True)
 
         # Loading the SAM model
@@ -80,11 +88,17 @@ class App:
         )
 
         # Showing the masks generated
-        fig, ax = plt.subplots()
-        plt.imshow(color_enhanced(self.img_gray, self.gt_mask, masks[0]), cmap='gray')
+        self.fig, ax = plt.subplots(1, 2)
+        ax[0].imshow(color_enhanced(self.img_gray, self.gt_mask, masks[0]), cmap='gray')
+        ax[0].set_title(
+            'Green: Truth Red: Predic Yellow: Inter')
         # plt.imshow(self.img_rgb)
         # self.show_mask(masks[0], ax=ax)
         # self.show_points(coords=np.array(self.input_coords), labels=np.array(self.input_labels), ax=ax)
+        # Showing the points selected
+        ax[1].imshow(cv2.cvtColor(self.img_points_selected, cv2.COLOR_BGR2RGB))
+        ax[1].set_title('Green: In Red: Background')
+        plt.suptitle(f'Prediction for the : {os.path.basename(self.image_path)}')
         plt.axis('on')
         plt.show()
 
@@ -94,15 +108,16 @@ class App:
         self.input_labels = []  # of the previous prompt
         window_name = 'Window'
         key_for_exit_window = 'q'
-        img_deepcopy = copy.deepcopy(self.img)
+        self.img_points_selected = copy.deepcopy(self.img)
 
         cv2.namedWindow(window_name, cv2.WINDOW_KEEPRATIO)
 
         # displaying the image
-        cv2.imshow(window_name, img_deepcopy)
+        cv2.imshow(window_name, self.img_points_selected)
 
         # setting mouse handler for the image and calling the click_event() function
-        cv2.setMouseCallback(window_name, self.click_event, param={'window_name': window_name, 'image': img_deepcopy})
+        cv2.setMouseCallback(window_name, self.click_event,
+                             param={'window_name': window_name, 'image': self.img_points_selected})
 
         # wait for a specific key to be pressed or window closing to exit
         wait_time = 1000
@@ -139,11 +154,12 @@ class App:
         # checking for left mouse clicks
         if event == cv2.EVENT_LBUTTONDOWN:
             # displaying the coordinates with the point on the image window
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(params['image'], str(x) + ',' +
-                        str(y), (x, y), font,
-                        1, (0, 255, 0), 1)
-            cv2.circle(params['image'], (x, y), radius=0, color=(0, 0, 255), thickness=-1)
+            # font = cv2.FONT_HERSHEY_SIMPLEX
+            # cv2.putText(params['image'], str(x) + ',' +
+            #             str(y), (x, y), font,
+            #             1, (0, 255, 0), 1)
+            # cv2.circle(params['image'], (x, y), radius=0, color=(0, 255, 0), thickness=-1)
+            params['image'][y, x] = [0, 255, 0]
             cv2.imshow(params['window_name'], params['image'])
 
             # storing the point selected
@@ -153,11 +169,12 @@ class App:
         # checking for right mouse clicks
         if event == cv2.EVENT_RBUTTONDOWN:
             # displaying the coordinates with the point on the image window
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(params['image'], 'b : ' + str(x) + ',' +
-                        str(y), (x, y), font,
-                        1, (255, 0, 0), 1)
-            cv2.circle(params['image'], (x, y), radius=0, color=(0, 0, 255), thickness=-1)
+            # font = cv2.FONT_HERSHEY_SIMPLEX
+            # cv2.putText(params['image'], 'b : ' + str(x) + ',' +
+            #             str(y), (x, y), font,
+            #             1, (255, 0, 0), 1)
+            # cv2.circle(params['image'], (x, y), radius=0, color=(0, 0, 255), thickness=-1)
+            params['image'][y, x] = [0, 0, 255]
             cv2.imshow(params['window_name'], params['image'])
 
             # storing the point selected
@@ -181,6 +198,12 @@ class App:
         ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='red',
                    linewidth=1.25)
 
+    def img_save(self):
+        ts = datetime.datetime.now().strftime("%Y%m%d-%H.%M.%S")
+        nb_in = self.input_labels.count(1)
+        nb_out = self.input_labels.count(0)
+        self.fig.savefig(f"../results/{ts}--{nb_in}i-{nb_out}o--{os.path.basename(self.image_path)}")
+        print(f"Saved image to ../results/{ts}--{nb_in}i-{nb_out}o--{os.path.basename(self.image_path)}")
 
 if __name__ == "__main__":
     app = App()
