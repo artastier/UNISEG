e68f229e65e56a0ba83c302c3d6c859a91489b84
damien-gautier-nantes <144633393+damien-gautier-nantes@users.noreply.github.com>
2024-03-27 14:09:44 +0100

    Update and rename points_prompt_generation.py to Application.py

--- SAM/Application.py
+++ SAM/Application.py
@@ -1,49 +1,64 @@
-# importing the module
+# @author Damien GAUTIER
+# This python file corresponds to an application allowing to :
+# - Load the SAM predictor
+# - Choose the image to segment
+# - Select the points belonging to the mask (left click) and the points belonging to the background (right click)
+# - Predict the masks with those points prompt
+# - Save the figure with the masks predicted and the points chosen
+
+# The button to choose the image also starts the embedding which can take some time (around 20 or 25 seconds) and can
+# freeze your computer.
+# Once an image has been selected, you can predict masks as much as you like. But, the prediction is always made with
+# the last points selected with the window when you click on the Button : "Select points to prompt".
+# For selecting the points belonging to the mask use the left click and the right click for the points belonging
+# to the background.
+# Once you have selected all the points you wanted just close the window opened when clicking on "Select points to
+# prompt".
+
+# Color legend plotted :
+# POINTS :
+# - mask (left click) : green
+# - background (right click) : red
+# MASKS :
+# - ground truth mask : green
+# - predicted mask by SAM : red
+# - intersection of ground truth mask and predicted mask : yellow
+
+# imports
 # UI
 from tkinter import *
 from tkinter.filedialog import askopenfilename
-
 # SAM model
 from segment_anything import sam_model_registry, SamPredictor
-
 # Analysing
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import copy
 import skimage.io as io
-
 # for test
 import time
-
 # Compare ground_truth and prediction
 from Compare import color_enhanced
-from colorama import Fore
 import os
 import datetime
 
 
-#######
-# Les masks sorties par SAM sont de même dimension que l'image d'entrée, ils sont codés en booléen
-# c'est à dire que chaque pixel prend une valeur soit False(Background) soit True(Mask)
-#
-
 class App:
 
     def __init__(self):
-        # Variables for storing the prompts
+        # Variables for plotting and computing
         self.fig = None
         self.img_points_selected = None
         self.img_gray = None
         self.gt_mask = None
         self.mask_path = None
+        self.image_path = None
+        self.img = None
+        self.img_rgb = None
+        # Variables for storing the prompts
         self.input_coords = []
         self.input_labels = []
-
-        # Loading the image that will be studied by default
-        self.image_path = './images/dog.jpg'
-        self.img = cv2.imread(self.image_path, 1)
-        self.img_rgb = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
 
         # Creating the window for the app
         self.window = Tk()
@@ -54,20 +69,23 @@ class App:
 
         # Adding all the components
         self.frame = Frame(self.window)
+        # The button for choosing the image
+        img_choice_button = Button(self.frame, font=("Courrier", 25), bg='white',
+                               fg='#41B77F', text="Load an image", command=self.img_choice)
+        img_choice_button.pack(pady=25, fill=X)
         # The button for prompting points
         points_button = Button(self.frame, text="Select points to prompt", font=("Courrier", 25), bg='white',
                                fg='#41B77F',
                                command=self.points_selection)
         points_button.pack(pady=25, fill=X)
         # The button for predicting
-        predict_button = Button(self.frame, text="Predict", command=self.predict)
-        predict_button.pack()
-        # The button for choosing the image
-        img_choice_button = Button(self.frame, text="Load an image", command=self.img_choice)
-        img_choice_button.pack()
+        predict_button = Button(self.frame, font=("Courrier", 25), bg='white',
+                               fg='#41B77F', text="Predict", command=self.predict)
+        predict_button.pack(pady=25, fill=X)
         # The button for saving the result
-        img_save_button = Button(self.frame, text="Save the prediction", command=self.img_save)
-        img_save_button.pack()
+        img_save_button = Button(self.frame, font=("Courrier", 25), bg='white',
+                               fg='#41B77F', text="Save the prediction", command=self.img_save)
+        img_save_button.pack(pady=25, fill=X)
         self.frame.pack(expand=True)
 
         # Loading the SAM model
@@ -92,11 +110,9 @@ class App:
         ax[0].imshow(color_enhanced(self.img_gray, self.gt_mask, masks[0]), cmap='gray')
         ax[0].set_title(
             'Green: Truth Red: Predic Yellow: Inter')
-        # plt.imshow(self.img_rgb)
-        # self.show_mask(masks[0], ax=ax)
-        # self.show_points(coords=np.array(self.input_coords), labels=np.array(self.input_labels), ax=ax)
         # Showing the points selected
         ax[1].imshow(cv2.cvtColor(self.img_points_selected, cv2.COLOR_BGR2RGB))
+        self.show_points(coords=np.array(self.input_coords), labels=np.array(self.input_labels), ax=ax[1], marker_size=(72./self.fig.dpi)**2)
         ax[1].set_title('Green: In Red: Background')
         plt.suptitle(f'Prediction for the : {os.path.basename(self.image_path)}')
         plt.axis('on')
@@ -133,7 +149,7 @@ class App:
         self.input_coords = []
         self.input_labels = []
 
-        # Loading the following window
+        # Loading the following image
         self.image_path = askopenfilename()
         self.img = cv2.imread(self.image_path, 1)
         self.img_rgb = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
@@ -154,11 +170,6 @@ class App:
         # checking for left mouse clicks
         if event == cv2.EVENT_LBUTTONDOWN:
             # displaying the coordinates with the point on the image window
-            # font = cv2.FONT_HERSHEY_SIMPLEX
-            # cv2.putText(params['image'], str(x) + ',' +
-            #             str(y), (x, y), font,
-            #             1, (0, 255, 0), 1)
-            # cv2.circle(params['image'], (x, y), radius=0, color=(0, 255, 0), thickness=-1)
             params['image'][y, x] = [0, 255, 0]
             cv2.imshow(params['window_name'], params['image'])
 
@@ -169,11 +180,6 @@ class App:
         # checking for right mouse clicks
         if event == cv2.EVENT_RBUTTONDOWN:
             # displaying the coordinates with the point on the image window
-            # font = cv2.FONT_HERSHEY_SIMPLEX
-            # cv2.putText(params['image'], 'b : ' + str(x) + ',' +
-            #             str(y), (x, y), font,
-            #             1, (255, 0, 0), 1)
-            # cv2.circle(params['image'], (x, y), radius=0, color=(0, 0, 255), thickness=-1)
             params['image'][y, x] = [0, 0, 255]
             cv2.imshow(params['window_name'], params['image'])
 
@@ -193,10 +199,8 @@ class App:
     def show_points(self, coords, labels, ax, marker_size=1):
         pos_points = coords[labels == 1]
         neg_points = coords[labels == 0]
-        ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='green',
-                   linewidth=1.25)
-        ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='red',
-                   linewidth=1.25)
+        ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green',marker='o', s=marker_size)
+        ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red',marker='o', s=marker_size)
 
     def img_save(self):
         ts = datetime.datetime.now().strftime("%Y%m%d-%H.%M.%S")
